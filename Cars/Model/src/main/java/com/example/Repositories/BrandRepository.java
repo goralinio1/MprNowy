a8f38e888da4ffb897b2256f2b77e4520cd4829a
goralinio1 <162348408+goralinio1@users.noreply.github.com>
2025-01-20 11:46:16 +0100

    Add files via upload

---
+++ Cars/Model/src/main/java/com/example/Repositories/BrandRepository.java
@@ -0,0 +1,6 @@
+package com.example.Repositories;
+
+import com.example.Model.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<Brand, Long> {}
